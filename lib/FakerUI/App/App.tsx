32ed9a05de2515f0294b9b297164fd6def05d7f9
Chipsetjunkie <rahulpr.connect@gmail.com>
2025-05-14 11:17:01 +0530

    fix: css scoped

--- lib/FakerUI/App/App.tsx
+++ lib/FakerUI/App/App.tsx
@@ -8,32 +8,32 @@ import { loader } from "@monaco-editor/react";
 import { AppState } from "@lib/FakerUI/types";
 import PreviewPane from "@lib/FakerUI/container/PreviewPane";
 import SchemaPane from "@lib/FakerUI/container/SchemaPane";
-import "@lib/FakerUI/global.scss";
 import ControlDock from "@lib/FakerUI/components/CoreComponents/ControlDock";
 
-import 'reactflow/dist/style.css';
+import "reactflow/dist/style.css";
 import Canvas from "../components/FeatureComponents/Canvas";
 import useMobileView from "../hooks/useMobileView";
 
-
-
 function SchemaWrapped(props: { data: AppState }) {
-  const generatedData: AppState = props.data
-  return generatedData && <div className={styles.panel_root_container} style={{ paddingLeft: "40px" }}>
-    <div className={styles.panel_element_container}>
-      <SchemaPane
-        generatedData={generatedData}
-      />
-    </div>
-  </div>
+  const generatedData: AppState = props.data;
+  return (
+    generatedData && (
+      <div
+        className={styles.panel_root_container}
+        style={{ paddingLeft: "40px" }}
+      >
+        <div className={styles.panel_element_container}>
+          <SchemaPane generatedData={generatedData} />
+        </div>
+      </div>
+    )
+  );
 }
 
-
-
 export function FakerGUI() {
-  const isMobile = useMobileView()
+  const isMobile = useMobileView();
   const { triggerPopup } = useToast();
-  const [isEditorView, setIsEditorView] = useState(true)
+  const [isEditorView, setIsEditorView] = useState(true);
   const [generatedData, setGeneratedData] = useState<AppState>({
     count: 5,
     triggerLoad: false,
@@ -41,7 +41,6 @@ export function FakerGUI() {
     rootNode: rootNodeData,
     hasError: false,
   });
-
 
   function loadFromLocal() {
     const cache = localStorage.getItem("schema");
@@ -58,16 +57,14 @@ export function FakerGUI() {
     }
   }
 
-
   function togglePanel() {
-    setIsEditorView(prev => !prev)
+    setIsEditorView((prev) => !prev);
   }
-
 
   function validateAndGenerate() {
     const { mainNode, isValid } = isValidSchema(generatedData.rootNode);
     if (isValid) {
-      togglePanel()
+      togglePanel();
       handleGenerate();
     } else {
       setGeneratedData((prev) => ({
@@ -111,14 +108,14 @@ export function FakerGUI() {
   }
 
   function updateCount(rawCount: string) {
-    if (isNaN(+rawCount)) return
-    if (+rawCount > 99){
+    if (isNaN(+rawCount)) return;
+    if (+rawCount > 99) {
       triggerPopup("max datapoint supported is 99", "error");
-      return
+      return;
     }
 
-    const count = +rawCount
-    
+    const count = +rawCount;
+
     setGeneratedData((prev) => ({
       ...prev,
       count,
@@ -139,35 +136,40 @@ export function FakerGUI() {
     });
   }, []);
 
-
-  const showPreviewPanel = !isEditorView
+  const showPreviewPanel = !isEditorView;
 
   return (
-    <div className={styles.root_editor_container}>
-      <Canvas<AppState> node={SchemaWrapped} customNodeProps={generatedData} isMobile={isMobile}/>
-      {!showPreviewPanel ? <ControlDock
-        generateData={validateAndGenerate}
-        loadData={loadFromLocal}
-        updateCount={updateCount}
-        saveSchema={saveSchemaLocally}
-        count={generatedData.count}
-      /> :
-        <div className={styles.panel_root_container}>
-          <div className={styles.panel_element_container} style={{ justifyContent: "flex-end" }}>
-            <PreviewPane
-              onClose={() => setIsEditorView(true)}
-              copyContent={copyContent}
-              generatedData={generatedData}
-            />
+      <div className={styles.root_editor_container}>
+        <Canvas<AppState>
+          node={SchemaWrapped}
+          customNodeProps={generatedData}
+          isMobile={isMobile}
+        />
+        {!showPreviewPanel ? (
+          <ControlDock
+            generateData={validateAndGenerate}
+            loadData={loadFromLocal}
+            updateCount={updateCount}
+            saveSchema={saveSchemaLocally}
+            count={generatedData.count}
+          />
+        ) : (
+          <div className={styles.panel_root_container}>
+            <div
+              className={styles.panel_element_container}
+              style={{ justifyContent: "flex-end" }}
+            >
+              <PreviewPane
+                onClose={() => setIsEditorView(true)}
+                copyContent={copyContent}
+                generatedData={generatedData}
+              />
+            </div>
           </div>
-        </div>
-      }
-    </div>
+        )}
+      </div>
   );
 }
-
-
-
 
 export function App() {
   return (
